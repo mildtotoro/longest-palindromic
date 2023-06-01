551c28690fd751cbb21ee0c607ee4e9d05af9a78
mild pimrampha <pimarmpha@onework.dev>
2023-06-01 23:11:29 +0700

    implement find longest palindrome and test

---
+++ utils/palindrome.test.ts
@@ -0,0 +1,52 @@
+import {
+  findLongestPalindrome,
+  checkPalindrome,
+  findLongestString,
+} from "./palindrome";
+
+describe("findLongestPalindrome module", () => {
+  test.each([
+    { input: "cbbs", output: "bb" },
+    { input: "xiaav", output: "aa" },
+    { input: "redder", output: "redder" },
+    { input: "xxxxxxxxxxxxxxxxxxxxx", output: "xxxxxxxxxxxxxxxxxxxxx" },
+    { input: "", output: "" },
+    { input: "q", output: "q" },
+    { input: "qd", output: "q" },
+    { input: "sss", output: "sss" },
+    { input: "ssss", output: "ssss" },
+  ])("inline table", ({ input, output }) => {
+    expect(findLongestPalindrome(input)).toMatch(output);
+  });
+});
+
+describe("findLongestString module", () => {
+  test.each([
+    { input: ["cbbs", "", "asdweewqe"], output: "asdweewqe" },
+    { input: [], output: "" },
+    { input: [""], output: "" },
+    { input: ["d", "e"], output: "d" },
+    { input: ["d", "e", "dd"], output: "dd" },
+  ])("inline table", ({ input, output }) => {
+    expect(findLongestString(input)).toEqual(output);
+  });
+});
+
+describe("checkPalindrome module", () => {
+  test.each([
+    { input: "babad", output: false },
+    { input: "cbbs", output: false },
+    { input: "xiaav", output: false },
+    { input: "redder", output: true },
+    { input: "redderk", output: false },
+    { input: "zzzzz", output: true },
+    { input: "z", output: true },
+    { input: "zd", output: false },
+    { input: "dod", output: true },
+    { input: "doud", output: false },
+    { input: "ii", output: true },
+    { input: "iakjsdhaksjdhi", output: false },
+  ])("inline table", ({ input, output }) => {
+    expect(checkPalindrome(input)).toEqual(output);
+  });
+});
---
+++ utils/palindrome.ts
@@ -0,0 +1,101 @@
+/**
+ * findLongestPalindrome
+ * @param text
+ * @returns string
+ */
+export function findLongestPalindrome(text: string): string {
+  if (text.length === 0) return "";
+  if (text.length === 1) return text;
+  if (text.length === 2 && text[0] !== text[1]) return text[0];
+
+  const lastIndex = text.length - 1;
+  console.log("lastIndex", lastIndex);
+  let startIndex = 0;
+  let endIndex = 0;
+  let stringPalindromeOdds: string[] = [];
+  let textCheckPalindrome = ``;
+
+  // Note: check palindrome length odd
+  for (let centerIndex = 0; centerIndex < lastIndex; centerIndex++) {
+    startIndex = centerIndex - 1;
+    endIndex = centerIndex + 1;
+
+    textCheckPalindrome = ``;
+    while (startIndex >= 0 && endIndex <= lastIndex) {
+      let startChar = text[startIndex];
+      let centerChar = text[centerIndex];
+      let endChar = text[endIndex];
+
+      if (textCheckPalindrome === "") {
+        textCheckPalindrome = centerChar;
+      }
+
+      textCheckPalindrome = `${startChar}${textCheckPalindrome}${endChar}`;
+
+      if (checkPalindrome(textCheckPalindrome)) {
+        stringPalindromeOdds.push(textCheckPalindrome);
+        startIndex = startIndex - 1;
+        endIndex = endIndex + 1;
+      } else {
+        break;
+      }
+    }
+  }
+
+  // Note: check palindrome length even
+  let stringPalindromeEvens: string[] = [];
+  textCheckPalindrome = "";
+  for (let anchorIndex = 0; anchorIndex < lastIndex; anchorIndex++) {
+    startIndex = anchorIndex;
+    endIndex = startIndex + 1;
+    while (endIndex <= lastIndex && startIndex >= 0) {
+      const leftChar = text[startIndex];
+      const rightChar = text[endIndex];
+      textCheckPalindrome = `${leftChar}${textCheckPalindrome}${rightChar}`;
+      console.log("textCheckPalindrome", textCheckPalindrome);
+
+      if (checkPalindrome(textCheckPalindrome)) {
+        stringPalindromeEvens.push(textCheckPalindrome);
+        startIndex = startIndex - 1;
+        endIndex = endIndex + 1;
+
+        console.log("startIndex", startIndex);
+        console.log("endIndex", endIndex);
+      } else {
+        textCheckPalindrome = "";
+        break;
+      }
+    }
+  }
+
+  // console.log("stringPalindromeOdds", stringPalindromeOdds);
+  console.log("stringPalindromeEvens", stringPalindromeEvens);
+  return findLongestString([...stringPalindromeOdds, ...stringPalindromeEvens]);
+}
+
+export function findLongestString(list: string[]): string {
+  if (list.length === 0) return "";
+  let res = list[0];
+  list.forEach((text) => {
+    if (res.length < text.length) {
+      res = text;
+    }
+  });
+  return res;
+}
+
+export function checkPalindrome(text: string): boolean {
+  const lastIndexStart = Math.floor(text.length / 2);
+  let endIndexStart = text.length - 1;
+  let isPalindrome = false;
+  for (let i = 0; i <= lastIndexStart; i++) {
+    if (text[i] === text[endIndexStart]) {
+      endIndexStart = endIndexStart - 1;
+      isPalindrome = true;
+    } else {
+      isPalindrome = false;
+      break;
+    }
+  }
+  return isPalindrome;
+}
