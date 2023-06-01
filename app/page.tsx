e343bd674808420f4184c94b7f1bf3f6673b7d59
mild pimrampha <pimarmpha@onework.dev>
2023-06-02 00:00:05 +0700

    update colour

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
   }
 
   return (
-    <main className="min-h-screen items-center justify-center flex px-2">
-      <div className="max-w-full  basis-[500px]">
+    <main className="min-h-screen items-center justify-center flex px-2 text-gray-700">
+      <div className="max-w-full basis-[500px]">
         <h2 className="text-center py-6 uppercase font-bold text-lg">
           Find The Longest Palindrome
         </h2>
